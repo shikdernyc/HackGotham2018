2bd945ea39c93ebe545c454269f9c53641a0036f
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-04-28 06:10:51 -0400

    Updated functions to Java style syntax

--- data/Items.js
+++ data/Items.js
@@ -1,32 +1,35 @@
-House = function (picture, location, features, Advertiser) {
+function House(picture, location, features, Advertiser) {
     this.location = location; //array of lat, long
     this.picture = picture; //use url
     this.features = features;
     this.Advertiser = Advertiser;
-    this.distanceTo = function(house){
+    this.distanceTo = function (house) {
         const xDelta = house.location.lat - this.location.lat;
         const yDelta = house.location.long - this.location.long;
         return Math.sqrt(Math.pow(xDelta, 2) + Math.pow(yDelta, 2))
     };
-
-    this.comparedTo = function (house) {
-        return (this.location.lat - house.location.lat)*10 + (this.location.long - house.location.long);
-    };
-
-    this.toJSON = function(){
+    this.toJSON = function () {
         return JSON.stringify({location: this.location, picture: this.picture, features: this.features.toJSON()})
     }
-};
+}
 
-Location = function (lat, long) {
+function Location(lat, long) {
     this.lat = lat;
     this.long = long;
-};
+}
 
-Features = function (numOfBedRoom, squareFootage, accessibility, petFriendly, parking) {
+function Features(numOfBedRoom, squareFootage, accessibility, petFriendly, parking) {
     this.numOfBedRoom = numOfBedRoom;
     this.squareFootage = squareFootage;
     this.accessibility = accessibility;
     this.petFriendly = petFriendly;
     this.parking = parking;
+}
+
+console.dir(new Location(0,0));
+
+module.exports = {
+    House: House,
+    Location: Location,
+    Features: Features
 };
