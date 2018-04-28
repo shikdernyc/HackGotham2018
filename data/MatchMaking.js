bc4faa889fb7ecb2271b504fe5b2e95d26a8530b
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-04-28 06:11:36 -0400

    Added ways to search and sort through houses for fast and easy access

--- data/MatchMaking.js
+++ data/MatchMaking.js
@@ -1,17 +1,22 @@
+const House = require('./Items.js').House;
+const Location = require('./Items.js').Location;
+const Features = require('./Items.js').Features;
+
 let search = function(location, features, radius){
     // for each house within radius, return their values
 };
 
 let sortHouses = function(array){
     array.sort(function(a, b){
-        return (this.location.lat - house.location.lat)*10 + (this.location.long - house.location.long);
-
+        return (a.location.lat - b.location.lat)*10 + (a.location.long - b.location.long);
     })
 };
 
-let house1 = new House("example.com", new Location(0,0), new Features(4,100,true,true,true));
 let house2 = new House("example.com", new Location(0,1), new Features(4,100,true,true,true));
+let house1 = new House("example.com", new Location(0,0), new Features(4,100,true,true,true));
 
-let houses = [house1, house2]
+let houses = [house1, house2];
 
-console.log(sortHouses(houses));
+sortHouses(houses);
+
+console.log(houses);
