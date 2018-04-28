bff2a909b4d43807a7847aca081e9f35f06d19b5
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-04-28 19:04:49 -0400

    Reformatting

--- House.js
+++ House.js
@@ -1,14 +1,5 @@
 class House {
-    //TODO: Create a static House Array
     static houses = [];
-    //TODO: Create a static method for adding Houses to the Array
-    static addHouse(house){
-        House.houses.add(house);
-    }
-    static removeHouse(house) {
-        House.houses.splice(House.houses.indexOf(house), 1);
-    }
-    //TODO: Create a Search
     constructor(picture, location, features, advertiser) {
         this.location = location; //array of lat, long
         this.picture = picture; //use url
@@ -16,6 +7,22 @@ class House {
         this.Advertiser = advertiser;
     }
 
+    static addHouse(house){
+        House.houses.add(house);
+    }
+    static removeHouse(house) {
+        House.houses.splice(House.houses.indexOf(house), 1);
+    }
+    //TODO: Create a Search
+    static search(features, location){
+
+    }
+    static sortHouses() {
+        House.houses.sort(function (a, b) {
+            //TODO: Make sure it works with negative coordinates/
+            return (a.location.lat - b.location.lat) * 10 + (a.location.long - b.location.long);
+        })
+    }
     distanceTo(location) {
         const xDelta = house.location.lat - this.location.lat;
         const yDelta = house.location.long - this.location.long;
