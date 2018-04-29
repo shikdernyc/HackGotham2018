292e29cc578b3deecf1d455a440c71fe28223f8c
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-04-29 10:44:00 -0400

    Changed Advertiser to a object

--- User.js
+++ User.js
@@ -1,17 +1,12 @@
-User = function (location, radius) {
+Users = function (location, radius) {
     this.location = location;
     this.radius = radius;
     this.search = features => {return features;} //use external module for matching
 };
 
-Advertiser = (username, contact, houses) => {
-    this.location = username;
-    this.contact = contact;
-    this.houses = houses; //array
-    this.addHouse = function (house) {
-        houses.add(houses);
-    };
-    this.removeHouse = function (house){
-        house.remove(house);
-    }
+function Advertiser(name, phone){
+    this.name = name;
+    this.phone = phone;
 };
+
+module.exports = {Advertiser: Advertiser};
