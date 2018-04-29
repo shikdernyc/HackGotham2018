c4c19aae69dd2bb55a921aab271e8d612f10a773
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-04-29 10:44:25 -0400

    Handling Post Request for adding a house

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express        =        require("express");
-const bodyParser     =        require("body-parser");
-const app            =        express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const House = require('./House').House;
+const Feature = require("./Items").Features;
+const Advertiser = require("./User").Advertiser;
+const Location = require("./Items").Location;
+const LocationParser = require('./LocationParser').LocationParser;
+
+lp = new LocationParser('AIzaSyCReE5aXx_8MnoBINpWV38mDeVKSExZoyY');
 
 app.use(express.static('public'));
 const post = 3000;
@@ -16,9 +23,26 @@ app.use(bodyParser.json());
 app.get('/',function(req,res){
     //needs to be abs
     res.sendFile('C:\\Users\\shikd\\WebstormProjects\\HackGotham2018\\public\\main.html');
-    //__dirname : It will resolve to your project folder.
 });
 
 app.post('/', function (req, res) {
    // Get routes for post requests
+    console.log("received request");
+    body = req.body;
+    let features = new Feature(body.bed, body.utilities, body.prices, body.houseType, body.squareFootage, body.accessibility, body.amenities, body.parking);
+    let advertiser = new Advertiser(body.name, body.phone);
+    console.log(body.location);
+    lp.getCodedLocation(body.location, (coded, simple)=>{
+        console.log("Getting Location");
+        let hLocation = new LocationParser(coded.lat, coded.long);
+        let house = new House(hLocation, features, advertiser);
+        console.dir(house);
+        console.log("Creating House");
+        House.addHouse(house);
+        res.send("House added");
+    });
+    body = req.body;
+    // res.send(house.toJSON())
+    res.send("House added")
+
 });
